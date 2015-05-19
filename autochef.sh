b6840852746552c5a31af42f8c43420ba7c094c6
Jonathan Porta <jonathan@jonathanPorta.com>
2015-05-18 19:09:18 -0700

    Run yum upgrade on the socrates repo before running chef

--- autochef.sh
+++ autochef.sh
@@ -3,5 +3,6 @@
 curl https://packagecloud.io/install/repositories/jonathanporta/socrates/script.rpm.sh | bash
 
 yum install -y chef-dk socrates
+yum upgrade -y socrates
 
 chef-solo -c /opt/jonathanporta/socrates/solo.rb -j /opt/jonathanporta/socrates/solo.json
