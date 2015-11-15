d03615b879684d187d3c340c60341c6559286d1e
Jonathan Porta <jonathan@jonathanPorta.com>
2015-11-14 20:43:46 -0800

    Add samba config

--- recipes/_samba.rb
+++ recipes/_samba.rb
@@ -4,11 +4,25 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
+# Samba Server Daemon and Utils
 package 'samba' do
   action [:install]
+  notifies :create, 'file[/etc/samba/smb.conf]', :immediately
+end
+
+# Samba Client - needed for testing
+package 'samba-client' do
+  action [:install]
+  notifies :create, 'file[/etc/samba/smb.conf]', :immediately
+end
+
+# Dropoff the Samba config and kick the process
+file '/etc/samba/smb.conf' do
+  action :nothing
+  notifies :restart, 'service[smb]', :immediately
 end
 
 # Notice that the service is named smb, NOT smbd. Carry on.
 service 'smb' do
-  action [:enable, :start]
+  action [:enable, :restart]
 end
