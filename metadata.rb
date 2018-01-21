6df53667a81b00d8d18de9b37099dbe2ca943dd9
Jonathan Porta <jonathan@jonathanporta.com>
2018-01-21 06:59:27 -0700

    Bump Version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jonathan@jonathanPorta.com'
 license 'WTFPL'
 description 'Installs/Configures socrates'
 long_description 'Installs/Configures socrates'
-version '0.1.0'
+version '0.2.0'
 
 depends 'packagecloud'
 depends 'porta_user'
